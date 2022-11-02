7073ce7e51e341340ab4f24dbbac50dabb3daf07
Ilya Morin <moy.morin22@gmail.com>
2022-11-03 01:55:27 +0300

    Create MessageHandler.cs

---
+++ MessageHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using VkNet;
+using VkNet.Enums.SafetyEnums;
+using VkNet.Model.RequestParams;
+
+namespace VkOnTheWay
+{
+    internal class MessageHandler
+    {
+        public static Dictionary<long, long?> GetConversationsUnreadMessages(VkApi vkApi)
+        {
+            var conversationsResult = vkApi.Messages.GetConversations(new GetConversationsParams
+            {
+                Filter = GetConversationFilter.Unread
+            });
+            return conversationsResult.Items.ToDictionary(x => x.Conversation.Peer.Id, x => x.Conversation.UnreadCount);
+        }
+        
+    }
+}
